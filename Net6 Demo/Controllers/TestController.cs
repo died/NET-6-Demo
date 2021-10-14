d5833b28459d1f9c808b8e6fc6c87f3b3754db12
Died <doomdied@gmail.com>
2021-10-14 15:07:31 +0800

    add logError for better example

---
+++ Net6 Demo/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Net6_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _log;
+        public TestController(ILogger<TestController> log)
+        {
+            _log = log;
+        }
+
+        [HttpGet]
+        public dynamic Index()
+        {
+            var ex = new NotImplementedException();
+            _log.LogError(ex, "Error log test");
+            return Ok();
+        }
+    }
+}
